895706b141f313706bed3210aad54dc767cbf841
kimku-0112 <66875200+kimku-0112@users.noreply.github.com>
2021-08-27 10:15:41 +0900

    Update download_essential_package.sh

--- download_essential_package.sh
+++ download_essential_package.sh
@@ -2,16 +2,16 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt install curl # if you haven't already installed curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
-sudo apt update
+sudo apt -y update
 
-sudo apt install ros-melodic-desktop-full
+sudo apt -y install ros-melodic-desktop-full
 
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
-sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt -y install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
 
-sudo apt install python-rosdep
+sudo apt -y install python-rosdep
 	
 sudo apt -y  update
 sudo apt -y upgrade
